dee51a4ff05d01d1a31b4b23f7eae296714f8f68
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-21 11:59:11 +0100

    add oeuvre in database with formulaire

---
+++ traitement.php
@@ -0,0 +1,28 @@
+<?php
+$postData = $_POST;
+if (
+    !isset($postData['titre']) ||
+    !isset($postData['artiste']) ||
+    !isset($postData['image']) ||
+    !isset($postData['description']) 
+) {
+    echo('Il faut un titre, un artiste, une image et une description pour soumettre le formulaire.');
+    return;
+}
+$titre = $_POST['titre'];
+$artiste = $_POST['artiste'];
+$image = $_POST['image'];
+$description = $_POST['description'];
+$insertOeuvre = $mysqlClient->prepare('INSERT INTO oeuvres(titre, artiste, image, description) VALUES (:titre, :artiste, :image, :description)');
+$insertOeuvre-> execute(
+    [
+        'titre' => $titre,
+        'artiste' => $artiste,
+        'image' => $image,
+        'description' => $description,
+    ]
+    )or die(print_r($mysqlClient->errorInfo()));
+$_SESSION['SUCCESS_MESSAGE'] = "Oeuvre ajoutée avec succès !";
+header('Location: index.php');
+exit();
+?>
