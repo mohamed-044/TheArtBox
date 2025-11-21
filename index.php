235717ff0b8e3093ce20952f822c20ca47f19224
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-21 11:39:49 +0100

    include oeuvres in oeuvre and index

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
-    require_once(__DIR__ . '/header.php');
-    require_once(__DIR__ . '/oeuvres.php');
-    require_once(__DIR__ . '/bdd.php');
-    require_once(__DIR__ . '/bdd_connect.php');
+require_once(__DIR__ . '/header.php');
+require_once(__DIR__ . '/bdd_connect.php');
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$oeuvresStatement = $mysqlClient->prepare('SELECT * FROM oeuvres');
+$oeuvresStatement->execute();
+$oeuvres = $oeuvresStatement->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<div id="liste-oeuvres">
-    <?php foreach($oeuvres as $oeuvre): ?>
+
+<section id="liste-oeuvres">
+    <?php foreach ($oeuvres as $oeuvre): ?>
         <article class="oeuvre">
             <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
-                <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
-                <h2><?= $oeuvre['titre'] ?></h2>
-                <p class="description"><?= $oeuvre['artiste'] ?></p>
+                <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
+                <h2><?= htmlspecialchars($oeuvre['titre']) ?></h2>
+                <p><?= htmlspecialchars($oeuvre['artiste']) ?></p>
             </a>
         </article>
     <?php endforeach; ?>
-</div>
-<?php require 'footer.php'; ?>
+</section>
+
+<?php require_once('footer.php'); ?>
--- oeuvres.php
+++ oeuvres.php
@@ -1,9 +1,19 @@
 <?php
 
+require_once(__DIR__ . '/bdd_connect.php');
+
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    header('Location: index.php');
+    exit;
+}
+
+$id = intval($_GET['id']);
+
 $oeuvresStatement = $mysqlClient->prepare('SELECT * FROM oeuvres WHERE id = ?');
-$oeuvresStatement->execute([$_GET['id']]);
-$oeuvre = $oeuvresStatement->fetch();
-echo '<pre>';
-print_r($oeuvres);
-echo '</pre>';
-exit;
+$oeuvresStatement->execute([$id]);
+$oeuvre = $oeuvresStatement->fetch(PDO::FETCH_ASSOC);
+
+if (!$oeuvre) {
+    header('Location: index.php');
+    exit;
+}
