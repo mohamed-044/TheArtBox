139637900f2f006ab1cd7352d1de5f5c98dd4eb3
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-21 10:56:51 +0100

    database connect

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 
-const MYSQL_HOST = 'localhost';
-const MYSQL_PORT = 3307;
-const MYSQL_NAME = 'artbox';
-const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+// Connexion à la base de données
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "artbox";
